ef1017ef214edc19fc37d563b88c0a151f32f7ff
dalinec <dalinec123@gmail.com>
2024-07-16 12:30:24 +0200

    added aspect ratio component from shadcn

--- src/components/DesignConfigurator.tsx
+++ src/components/DesignConfigurator.tsx
@@ -9,7 +9,13 @@ const DesignConfigurator = ({
   imageUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
-  return <div>DesignConfigurator</div>;
+  return (
+    <div className="relative mb-20 mt-20 grid grid-cols-3 pt-20">
+      <div className="relative col-span-2 flex h-[37.5rem] w-full max-w-4xl items-center justify-center overflow-hidden rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-1">
+        <div className="pointer-events-none relative aspect-[896/1831] w-60 bg-opacity-50"></div>
+      </div>
+    </div>
+  );
 };
 
 export default DesignConfigurator;
